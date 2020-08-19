f869977f3811baf2cab8193e58d591a773f0f70e
Your Name <you@example.com>
2020-08-19 15:24:37 +0700

    make it better

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ function handleEvent(event) {
     const userId = event.source.userId;
     const payload = {
       type:"text",
-      text: "hello"
+      text: userId
     };
      return client.replyMessage(event.replyToken ,payload);
   }

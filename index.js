27dd624991895433c1b42e3df56808b9656d3ec4
Your Name <you@example.com>
2020-08-19 13:03:21 +0700

    make it better

--- index.js
+++ index.js
@@ -33,13 +33,14 @@ function handleEvent(event) {
   if (event.type !== 'message' || event.message.type !== 'text') {
     // ignore non-text-message event
     return Promise.resolve(null);
+  }else if(event.message.type === "text" || event.message.text === "Hello"){
+    const payload = {
+      type:"text",
+      text:"Hello from Heroku Server."
+    };
+
+     return client.replyMessage(event.replyToken , payload);
   }
-
-  // create a echoing text message
-  const echo = { type: 'text', text: event.message.text };
-
-  // use reply API
-  return client.replyMessage(event.replyToken, echo);
 }
 
 // listen on port

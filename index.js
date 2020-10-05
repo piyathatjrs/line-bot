f715fe2806f5bc1fdec25a46ba973bb5568d334f
Your Name <you@example.com>
2020-10-05 17:35:14 +0700

    make it better

--- index.js
+++ index.js
@@ -45,38 +45,3 @@ function handleEvent(event) {
   }
 }
 
-// listen on port
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`listening on ${port}`);
-});
-
-
-
-const functions = require("firebase-functions");
-const request = require("request-promise");
-
-const LINE_MESSAGING_API = "https://api.line.me/v2/bot/message";
-const LINE_HEADER = {
-  "Content-Type": "application/json",
-  "Authorization": "Bearer <CHANNEL-ACCESS-TOKEN>"
-};
-
-exports.BasicMessage = functions.https.onRequest((req, res) => {
-  return request({
-    method: "POST",
-    uri: `${LINE_MESSAGING_API}/push`,
-    headers: LINE_HEADER,
-    body: JSON.stringify({
-      to: "U1222b3980cb7a3c2ef703b59a4784d3d",
-      messages: [{
-          type: "text",
-          text: "LINE \uDBC0\uDC84 x \uDBC0\uDCA4 Firebase"
-      }]
-    })
-  }).then(() => {
-      return res.status(200).send("Done");
-  }).catch(error => {
-      return Promise.reject(error);
-  });
-});

ac5f488140c8716eb5f6e03161ab4449e2ad801f
Your Name <you@example.com>
2020-10-05 20:07:36 +0700

    make it better

--- index.js
+++ index.js
@@ -3,7 +3,10 @@
 const line = require('@line/bot-sdk');
 const express = require('express');
 // create LINE SDK config from env variables
-
+const config = {
+  channelAccessToken: "Trkk6NZiJgsrk7qFr1klaMA32EKjvqmigI48XGk6hKPw2AIVyxw6IU6tdj5rOBQRrU+H/dm0IZoQNoqtVsjfttxAlmTwoVggvUBGgyDRaFqT6ZVQTeN99kqaDJx9ycxKTGYUXwxuxq7k6hv/qVkJvQdB04t89/1O/w1cDnyilFU=",
+  channelSecret: "197977fb67765940cb7528882342c8d2",
+};
 
 // create LINE SDK client
 const client = new line.Client(config);
@@ -25,7 +28,7 @@ app.post('/callback', line.middleware(config), (req, res) => {
 });
 // event handler
 function handleEvent(event) {
-
+  
   if (event.type !== 'message' || event.message.type !== 'text') {
     // ignore non-text-message event
     return Promise.resolve(null);

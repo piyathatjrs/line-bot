4d9273d323d7da2a71ce43b2428608d0c1af9380
Your Name <you@example.com>
2020-10-11 19:30:52 +0700

    make it better

--- index.js
+++ index.js
@@ -48,238 +48,58 @@ function handleEvent(event) {
     const userId = event.source.userId;
 
     const payload = {
+     
+
       type: "flex",
       altText: "Flex Message",
       contents: {
-        type: "carousel",
-        contents: [
-          {
-            type: "bubble",
-            hero: {
-              type: "image",
-              url:
-                "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_5_carousel.png",
-              size: "full",
-              aspectRatio: "20:13",
-              aspectMode: "cover",
+        type: "bubble",
+        direction: "ltr",
+        header: {
+          type: "box",
+          layout: "vertical",
+          contents: [
+            {
+              type: "text",
+              text: "เชื่อมต่อLINEกับทางเซ้ปไซต์",
+              align: "center",
             },
-            body: {
-              type: "box",
-              layout: "vertical",
-              spacing: "sm",
-              contents: [
-                {
-                  type: "text",
-                  text: "Arm Chair, White",
-                  size: "xl",
-                  weight: "bold",
-                  wrap: true,
-                },
-                {
-                  type: "box",
-                  layout: "baseline",
-                  contents: [
-                    {
-                      type: "text",
-                      text: "$49",
-                      flex: 0,
-                      size: "xl",
-                      weight: "bold",
-                      wrap: true,
-                    },
-                    {
-                      type: "text",
-                      text: ".99",
-                      flex: 0,
-                      size: "sm",
-                      weight: "bold",
-                      wrap: true,
-                    },
-                  ],
-                },
-              ],
+          ],
+        },
+        hero: {
+          type: "image",
+          url:
+            "https://developers.line.biz/assets/images/services/bot-designer-icon.png",
+          size: "full",
+          aspectRatio: "1.51:1",
+          aspectMode: "fit",
+        },
+        body: {
+          type: "box",
+          layout: "vertical",
+          contents: [
+            {
+              type: "text",
+              text: "Body",
+              align: "center",
             },
-            footer: {
-              type: "box",
-              layout: "vertical",
-              spacing: "sm",
-              contents: [
-                {
-                  type: "button",
-                  action: {
-                    type: "uri",
-                    label: "Add to Cart",
-                    uri: "https://linecorp.com",
-                  },
-                  style: "primary",
-                },
-                {
-                  type: "button",
-                  action: {
-                    type: "uri",
-                    label: "Add to whishlist",
-                    uri: "https://linecorp.com",
-                  },
-                },
-              ],
+          ],
+        },
+        footer: {
+          type: "box",
+          layout: "horizontal",
+          contents: [
+            {
+              type: "button",
+              action: {
+                type: "uri",
+                label: "กดปุ่ม",
+                uri: "http://localhost/project/Login_v16/login_add_line.php?id_line="+userId+"",
+              },
             },
-          },
-          {
-            type: "bubble",
-            hero: {
-              type: "image",
-              url:
-                "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_6_carousel.png",
-              size: "full",
-              aspectRatio: "20:13",
-              aspectMode: "cover",
-            },
-            body: {
-              type: "box",
-              layout: "vertical",
-              spacing: "sm",
-              contents: [
-                {
-                  type: "text",
-                  text: "Metal Desk Lamp",
-                  size: "xl",
-                  weight: "bold",
-                  wrap: true,
-                },
-                {
-                  type: "box",
-                  layout: "baseline",
-                  flex: 1,
-                  contents: [
-                    {
-                      type: "text",
-                      text: "$11",
-                      flex: 0,
-                      size: "xl",
-                      weight: "bold",
-                      wrap: true,
-                    },
-                    {
-                      type: "text",
-                      text: ".99",
-                      flex: 0,
-                      size: "sm",
-                      weight: "bold",
-                      wrap: true,
-                    },
-                  ],
-                },
-                {
-                  type: "text",
-                  text: "Temporarily out of stock",
-                  flex: 0,
-                  margin: "md",
-                  size: "xxs",
-                  color: "#FF5551",
-                  wrap: true,
-                },
-              ],
-            },
-            footer: {
-              type: "box",
-              layout: "vertical",
-              spacing: "sm",
-              contents: [
-                {
-                  type: "button",
-                  action: {
-                    type: "uri",
-                    label: "Add to Cart",
-                    uri: "https://linecorp.com",
-                  },
-                  flex: 2,
-                  color: "#AAAAAA",
-                  style: "primary",
-                },
-                {
-                  type: "button",
-                  action: {
-                    type: "uri",
-                    label: "Add to wish list",
-                    uri: "https://linecorp.com",
-                  },
-                },
-              ],
-            },
-          },
-          {
-            type: "bubble",
-            body: {
-              type: "box",
-              layout: "vertical",
-              spacing: "sm",
-              contents: [
-                {
-                  type: "button",
-                  action: {
-                    type: "uri",
-                    label: "See more",
-                    uri: "https://linecorp.com",
-                  },
-                  flex: 1,
-                  gravity: "center",
-                },
-              ],
-            },
-          },
-        ],
+          ],
+        },
       },
-
-      // type: "flex",
-      // altText: "Flex Message",
-      // contents: {
-      //   type: "bubble",
-      //   direction: "ltr",
-      //   header: {
-      //     type: "box",
-      //     layout: "vertical",
-      //     contents: [
-      //       {
-      //         type: "text",
-      //         text: "เชื่อมต่อLINEกับทางเซ้ปไซต์",
-      //         align: "center",
-      //       },
-      //     ],
-      //   },
-      //   hero: {
-      //     type: "image",
-      //     url:
-      //       "https://developers.line.biz/assets/images/services/bot-designer-icon.png",
-      //     size: "full",
-      //     aspectRatio: "1.51:1",
-      //     aspectMode: "fit",
-      //   },
-      //   body: {
-      //     type: "box",
-      //     layout: "vertical",
-      //     contents: [
-      //       {
-      //         type: "text",
-      //         text: "Body",
-      //         align: "center",
-      //       },
-      //     ],
-      //   },
-      //   footer: {
-      //     type: "box",
-      //     layout: "horizontal",
-      //     contents: [
-      //       {
-      //         type: "button",
-      //         action: {
-      //           type: "uri",
-      //           label: "กดปุ่ม",
-      //           uri: "http://localhost/project/Login_v16/login_add_line.php?id_line="+userId+"",
-      //         },
-      //       },
-      //     ],
-      //   },
-      // },
     };
 
     return client.replyMessage(event.replyToken, payload);
